e9a2be1c1425d5e4d89bc483ce68ceb4cf46c3a5
Mina Chuong <mina.chuong@gmail.com>
2017-08-02 14:11:49 -0700

    Lesson 4: rendering with external data; using lifecycle event method 'componentDidMount' to fetch from ContactsAPI so that contacts are no longer hard coded into the component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 //const contacts = [
 //  {
@@ -25,27 +26,18 @@ import ListContacts from './ListContacts'
 //initializing state within component
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   }
+  
+  //this is a lifecycle event method that fetches data after the component has mounted
+  //technically the first time it mounted, there should have displayed no contacts
+  //then the fetch occurs and the setState calls the re-render
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+
   //function that gets passed into ListContacts as a prop
   //receives the contact prop from ListContacts 
     //calls the current state; looks at the contacts property
