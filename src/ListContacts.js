b938423db369ac2f259709b01eef51f4d295a424
Mina Chuong <mina.chuong@gmail.com>
2017-08-03 12:28:03 -0700

    using Link component from React Router and wrapped BrowserRouter around App component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
-
+import { Link } from 'react-router-dom'
 
 // stateless functional component
 //invokes onDeleteContact prop in parent App component
@@ -79,11 +79,10 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a
-            href='#create'
-            onClick={this.props.onNavigate}
+          <Link
+            to='/create'
             className='add-contact'
-          >Add Contact></a>  
+          >Add Contact></Link>  
         </div>
         { showingContacts.length !== contacts.length && (
           <div className='showing-contacts'>
