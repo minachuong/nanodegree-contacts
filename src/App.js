f4ba24a4b5a3db44b480ce82cb1f30037cc7856f
Mina Chuong <mina.chuong@gmail.com>
2017-08-03 13:16:47 -0700

    using route component to match exact URLs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
@@ -56,18 +57,16 @@ class App extends Component {
   render() {
     return (
       <div className ='app'>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts 
-           onDeleteContact={this.removeContact} 
-           contacts={this.state.contacts} 
-           onNavigate={() => {
-             this.setState({ screen: 'create' })
-           }}
+            onDeleteContact={this.removeContact} 
+            contacts={this.state.contacts} 
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }}
           />
-        )}  
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
+        )}/>
+        <Route path='/create' component={CreateContact}/>
       </div>
     )
   }
