f4ec471d0cd83d02864eda04530f6979a2f289c0
Mina Chuong <mina.chuong@gmail.com>
2017-08-03 18:10:40 -0700

    passing history object into route component for createContact so that when a contact is added, user is redirected back to the list of contacts

--- src/App.js
+++ src/App.js
@@ -53,6 +53,15 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+  
   //onDeleteContact is invoke in the ListContact component
   render() {
     return (
@@ -63,7 +72,14 @@ class App extends Component {
             contacts={this.state.contacts} 
           />
         )}/>
-        <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
